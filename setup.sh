b730ef0eeb87f0cfa94f6df072aaedc3f328dce1
Gyeongchan Yun <rugyoon@gmail.com>
2019-10-04 19:35:26 +0900

    Change overwrite action

--- setup.sh
+++ setup.sh
@@ -4,19 +4,23 @@ if [ -f ~/.tmux.conf ] ; then
     read -p "[CAUTION] .tmux.conf already exists. Do you want to overwrite [Y/n]? " yn
     case $yn in
         "") 
-            cat tmux.conf >> ~/.tmux.conf
+            cp tmux.conf ~/.tmux.conf
             tmux source-file ~/.tmux.conf
             ;;
         [Yy]*)
-            cat tmux.conf >> ~/.tmux.conf
+            cp tmux.conf ~/.tmux.conf
             tmux source-file ~/.tmux.conf
             ;;
         [Nn]*)
             mv ~/.tmux.conf ~/tmp.tmux.conf
-            echo "[INFO] Change .tmux.conf to tmp.tmux.conf"
+            echo "[INFO] Change existing file to tmp.tmux.conf"
             cp tmux.conf ~/.tmux.conf
             tmux source-file ~/.tmux.conf
             ;;
     esac
-    echo "[INFO] Success to load tmux configuration."
+else
+    cp tmux.conf ~/.tmux.conf
+    tmux source-file ~/.tmux.conf
 fi
+
+echo "[INFO] Success to load my tmux configuration."
